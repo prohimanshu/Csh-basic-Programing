9fb92b5bc2e942f3590236e6ccf19d5600701454
Himanshu <coderhimanshu@outlook.com>
2018-11-20 17:32:31 +0530

    Start Programming Hello
    program hello with hello test 20Nov2018

---
+++ Csh-basic-Programing/Csh-basic-Programing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Hello
+{
+    static void Main(String [] a)
+    {
+        Console.Write("hello");
+        Console.ReadLine();
+    }
+}

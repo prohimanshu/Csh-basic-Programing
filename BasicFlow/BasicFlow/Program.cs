3867d5746f799a3adb8fd78364b5415298aef1f3
Himanshu <coderhimanshu@outlook.com>
2018-11-21 12:24:46 +0530

    program 2 BasicFlow

---
+++ BasicFlow/BasicFlow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace BasicFlow
+{
+    class Program
+    {
+        static void Main (string[] a)
+        {
+            Console.WriteLine("I'm in Main Class");
+            Run();
+            Console.WriteLine("Again i'm in class main");
+            Console.ReadLine();
+        }
+        static void Run()
+        {
+            Console.WriteLine("i'm in sub class 'static void Run'");
+        }
+    }
+    public class Computer
+    {
+        //Console.WriteLine("i'm in class 'Computer'");
+    }
+    class Enemy
+    {
+        //Console.WriteLine("I'm in 'class Enemy'");
+    }
+}
